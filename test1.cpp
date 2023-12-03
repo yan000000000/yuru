7db8a1d5dee4dca6180a6574a49b32890477b3e6
yanlin <kagakuzenku@gmail.com>
2023-12-03 11:54:04 +0800

    update

--- test1.cpp
+++ test1.cpp
@@ -3,6 +3,12 @@
 int main()
 {
 	std::cout << "hello, world" << std::endl;
+	std::cout << "my name is steve, i con't wait to join in the coding world" << std::endl;
+	std::cout << "test 1 changed" << std::endl;
 
+	int a = 52;
+	int b = 10;
+	std::cout << a * b;
+	std::cout << " c plus plus" << std::endl;
 	return 0;
 }
