5f61687c2358f0798b8124858fbe23465b576cd3
yanlin <kagakuzenku@gmail.com>
2023-12-05 14:19:07 +0800

    update test1.cpp to hello world

--- test1.cpp
+++ test1.cpp
@@ -3,13 +3,6 @@
 int main()
 {
 	std::cout << "hello, world" << std::endl;
-	std::cout << "my name is steve, i con't wait to join in the coding world" << std::endl;
-	std::cout << "test 1 changed" << std::endl;
 
-	int a = 52;
-	int b = 10;
-	std::cout << a * b;
-	std::cout << " c plus plus" << std::endl;
-	std::cout << "python" << std::endl;
 	return 0;
 }
